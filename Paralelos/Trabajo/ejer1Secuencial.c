7f18296b6c7be94a0e760a92aa0bd4256c6aff70
komartinez <kottymartinez@gmail.com>
2018-05-12 18:15:40 -0300

    ejercicio 1 secuencial terminado

--- Paralelos/Trabajo/ejer1Secuencial.c
+++ Paralelos/Trabajo/ejer1Secuencial.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+
+int N;
+
 //Para calcular tiempo
 double dwalltime(){
         double sec;
@@ -11,14 +14,14 @@ double dwalltime(){
         sec = tv.tv_sec + tv.tv_usec/1000000.0;
         return sec;
 }
-int N;
+
 int main(int argc,char* argv[]){
 
  double *A,*B,*C,*D,*LT,*M,*AB,*ABC,*LB,*LBD;
  double sumab, sumal, promL, promB;
  int i,j,k;
  int check = 1;
- double timetick;
+ double timetick, tiempoFinal;
  int cantTriangular;
 
  //Controla los argumentos al programa
@@ -26,8 +29,7 @@ int main(int argc,char* argv[]){
     printf("\nUsar: %s n\n  n: Dimension de la matriz (nxn X nxn)\n", argv[0]);
     exit(1);
   }
- 
- cantTriangular = N*(N+1)/2;
+
 
  //Aloca memoria para las matrices
  A=(double*)malloc(sizeof(double)*N*N);
@@ -54,19 +56,15 @@ int main(int argc,char* argv[]){
     LBD[i*N+j]=0;
 
     if(i<=j){
-     LT[i+N*j]=1; //Cargar triangular inferior
+     LT[i+N*j]=1; //Carga triangular inferior
     }else{
      LT[i+N*j]=0; 
     }
-
-    printf("matriz triangular, valor en %d : %f\n", i+N*j, LT[i+N*j]); 
+    
+    //printf("matriz triangular, valor en %d : %f\n", i+N*j, LT[i+N*j]); 
   }
  }
-
- /*for (i = 0; i < cantTriangular; i++)
- {
-   L[i] = 1;
- }*/
+//printf("\n matriz triangular:\n"); 
 
  //Realiza la multiplicacion 
  timetick = dwalltime();
@@ -80,13 +78,14 @@ for(i=0;i<N;i++){
 }
 
 //multiplicación matriz triangular
+
+printf("\n");
 for (i = 0; i < N; i++){
   for (j = 0; j < N; j++){
-    for (k = 0; k <= i; k++){
-      //printf("zona matriz triangular %d\n", i*N+k);
-      //printf("zona matriz comun: %d\n", k+N*j);
+    for (k = 0; k < N; k++){
+      LB[i*N+j]=LB[i*N+j] + LT[i*N+k]*B[k+j*N];
+      //printf("matriz: %d\n", k+j*N);
     }
-    //printf("fin bucle k\n");
   }
 }
 
@@ -94,11 +93,8 @@ for(i=0;i<N;i++){
   for(j=0;j<N;j++){
    for(k=0;k<N;k++){
     ABC[i*N+j]=ABC[i*N+j] + AB[i*N+k]*C[k+j*N];
-    //printf("A %d\n", i*N+k);
-    printf("B %d\n", k+j*N);
     LBD[i*N+j]=LBD[i*N+j] + LB[i*N+k]*D[k+j*N];
    }
-   printf("fin k\n");
   }
 }
 
@@ -118,26 +114,23 @@ promB= sumab/(N*N);
 for (i=0; i<N; i++){
   for (j = 0; j < N; j++){
     M[i*N+j] = promL*ABC[i*N+j] + promB*LBD[i*N+j];
-    //printf("valor matriz m en %d : %f\n", i*N+j, M[i*N+j]);
   }
 }
 
-printf("promedio matriz B: %f\n", promB);
+tiempoFinal = dwalltime() - timetick;
 
-printf("Tiempo en segundos %f\n", dwalltime() - timetick);
 
- //Verifica el resultado
-/* for(i=0;i<N;i++){
-  for(j=0;j<N;j++){
-   check = check && (C[i*N+j]==N);
+//Imprimo matriz
+for (int i = 0; i < N; i++){
+  for (int j = 0; j < N; j++){
+    printf("valor matriz m en %d : %f\n", i*N+j, M[i*N+j]);
   }
- }
+}
 
- if(check){
-  printf("Multiplicacion de matrices correcta\n");
- }else{
-  printf("Multiplicacion de matrices erroneo\n");
- }*/
+//printf("promedio matriz B: %f\n", promB);
+
+printf("Tiempo en segundos %f\n", tiempoFinal);
+
  
  free(A);
  free(B);
