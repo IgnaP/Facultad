96077407559bcdf3dfe62f98162090edb476b362
Ignacio <ignacioposada@hotmail.com>
2018-05-31 16:58:37 -0300

    Add files via upload

--- Paralelos/MPIprueba.c
+++ Paralelos/MPIprueba.c
@@ -6,25 +6,26 @@
 
 int main(int argc, char *argv[]){
 	int cantidad, identificador;
-	int num;
-	int resp;
+	int num=999;
+	int resp=999;
 	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size (MPI_COMM_WORLD, &cantidad);
 	MPI_Comm_rank (MPI_COMM_WORLD, &identificador);
+	MPI_Status estado;
 	if (identificador==0) {
-		palabra='Prueba';
-		MPI_Send(num,1,MPI_INIT,1,1,MPI_COMM_WORLD);
-		MPI_Recv(resp,1,MPI_INIT,1,2,MPI_COMM_WORLD);
-		printf("%i",resp);
+		num=10;
+		MPI_Send(&num,1,MPI_INT,1,1,MPI_COMM_WORLD);
+		MPI_Recv(&resp,1,MPI_INT,1,1,MPI_COMM_WORLD,&estado);
+		printf("%i\n",resp);
 	}else{
-		MPI_Recv(palabra,1,MPI_INIT,0,2,MPI_COMM_WORLD);
+		MPI_Recv(&num,1,MPI_INT,0,1,MPI_COMM_WORLD,&estado);
 		if (num==10) {
 			resp=0;
 		}else{
 			resp=1;
 		}
-		MPI_Send(resp,1,MPI_INIT,0,1,MPI_COMM_WORLD);
+		MPI_Send(&resp,1,MPI_INT,0,1,MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
 }
