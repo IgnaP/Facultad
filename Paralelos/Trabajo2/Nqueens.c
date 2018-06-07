470dd5033c25539e316676047117753f25b51d78
Ignacio <ignacioposada@hotmail.com>
2018-06-07 16:51:39 -0300

    Add files via upload

--- Paralelos/Trabajo2/Nqueens.c
+++ Paralelos/Trabajo2/Nqueens.c
@@ -6,16 +6,26 @@
 
 #define MAX_N 16
 
+double dwalltime()
+{
+	double sec;
+	struct timeval tv;
+
+	gettimeofday(&tv,NULL);
+	sec = tv.tv_sec + tv.tv_usec/1000000.0;
+	return sec;
+}
+
 int check_acceptable(int queen_rows[MAX_N], int n){
 	int i, j;
 	for (i = 0; i < n; i++){
 		for (j = i+1; j < n; j++){
 			// two queens in the same row => not a solution!
-			if (queen_rows[i] == queen_rows[j]) return 0;
-			
+			if (queen_rows[i] == queen_rows[j]) 
+				return 0;
 			// two queens in the same diagonal => not a solution!
 			if (queen_rows[i] - queen_rows[j] == i - j || queen_rows[i] - queen_rows[j] == j - i)
-			    return 0;
+				return 0;
 		}
 	}
 	return 1;
@@ -23,22 +33,19 @@ int check_acceptable(int queen_rows[MAX_N], int n){
 
 int main(int argc, char* argv[])
 {
-    int n;
+    int n,i,iter;
     int max_iter = 1;
-    
     double start_time, end_time;
     int number_solutions = 0;
-	{
-	int num_workers;
-        int i;
 	
         n = (argc > 1) ? atoi(argv[1]) : 8;
 	        
         for (i = 0; i < n; i++){
             max_iter *= n;
         }
-    }
-	int iter;
+
+	start_time = dwalltime();
+
 	for (iter = 0; iter < max_iter; iter++){
 		int code = iter;
 		int i;
@@ -47,7 +54,6 @@ int main(int argc, char* argv[])
 		// we only generate configurations where there's only one queen per collumn
 		for (i = 0; i < n; i++){
 			queen_rows[i] = code % n;
-			
 			code /= n;
 		}
 		if (check_acceptable(queen_rows, n)){
@@ -66,10 +72,10 @@ int main(int argc, char* argv[])
 			printf("\n");
 		}
 	}
+	end_time = dwalltime();
     // print results
     printf("The execution time is %g sec\n", end_time - start_time);
     printf("Number of found solutions is %d\n", number_solutions);
     
 	return 0;
 }
-
